b33215172710d5e2bcb5fd8cf1d05fd1e221247f
max behling <elmachomax123@gmail.com>
2018-03-26 21:47:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,23 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(e) {
-  // Write your JavaScript code inside the init() function
-const code = parseInt(e.detail || e.which);
-if (code === init[e]) {
-  code++;
-  if (e === code.length) {
-    alert('hurray!');
-    code = 0
-  } else {
-    code = 0
+
+const konamiCode = [65, 66, 67];
+let index = 0;
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+  if (key === konamiCode[index]) {
+    index++;
+    if (index === konamiCode.length) {
+      alert("Hurray!");
+      
+      index = 0;
+    
+    } else {
+      index = 0;
+      alert("wrong a second time!")
+    }
   }
 }
-} 
+
+
+
+
